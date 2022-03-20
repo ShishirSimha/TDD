aabd372bcb2483398cbc2ee58b2ebf8eb59197d5
Shishir Simha <simbashishir@hotmail.com>
2022-03-20 22:28:27 +0530

    Final commit with custom delimiters and code formatted as per coding standards

--- Calculator/src/com/incubyte/calculator/Calculator.java
+++ Calculator/src/com/incubyte/calculator/Calculator.java
@@ -1,38 +1,41 @@
 package com.incubyte.calculator;
 
 import java.util.ArrayList;
-import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
-	
+
 	/**
 	 * Method that adds up two or more numbers
+	 * 
 	 * @param nums
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static int add(String nums) throws Exception {
 		int answer = 0;
-		//split the numbers
+		// split the numbers
 		String[] numbers = splitNumbers(nums);
-		
-		//throw exception if 1 or more numbers are negative
+
+		// throw exception if 1 or more numbers are negative
 		throwNegativeNumbersIfAny(numbers);
-		
-		//compute answer
+
+		// compute answer
 		answer = computeAnswer(numbers);
 		return answer;
 	}
-	
+
 	/**
 	 * Method that adds all the numbers
+	 * 
 	 * @param numbers
 	 * @return
 	 */
 	private static int computeAnswer(String[] numbers) {
 		// TODO Auto-generated method stub
 		int answer = 0;
-		for(String number : numbers) {
+		for (String number : numbers) {
 			int num = Integer.parseInt(number.trim());
 			answer += num;
 		}
@@ -41,18 +44,47 @@ public class Calculator {
 
 	/**
 	 * Method that splits numbers based on a splitter.
+	 * 
 	 * @param nums
 	 * @return
 	 */
 	private static String[] splitNumbers(String nums) {
-		//for empty string return 0;
-		if(nums.isEmpty()) {
+		// for empty string return 0;
+		if (nums.isEmpty()) {
 			return new String[0];
+		} else if (hasCustomDelimiter(nums)) {
+			return splitByCustomDelimiter(nums);
 		}
-		//else split numbers using comma or \n
+		// else split numbers using comma or \n
 		return nums.split(",|\n");
 	}
-	
+
+	/**
+	 * Method that splits via custom delimiter
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	private static String[] splitByCustomDelimiter(String nums) {
+		// TODO Auto-generated method stub
+		Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(nums);
+		matcher.matches();
+		String customDelim = matcher.group(1);
+		String num = matcher.group(2);
+		return num.split(Pattern.quote(customDelim));
+	}
+
+	/**
+	 * Method that checks for custom delimiter;
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	private static boolean hasCustomDelimiter(String nums) {
+		// TODO Auto-generated method stub
+		return nums.startsWith("//");
+	}
+
 	/**
 	 * Method that throws exception on negative numbers
 	 * 
@@ -62,13 +94,13 @@ public class Calculator {
 	private static void throwNegativeNumbersIfAny(String[] nums) throws Exception {
 		ArrayList<String> negative = new ArrayList<String>();
 		int size = nums.length;
-		for(int i=0; i<size; i++) {
-			if(Integer.parseInt(nums[i].trim())<0) {
+		for (int i = 0; i < size; i++) {
+			if (Integer.parseInt(nums[i].trim()) < 0) {
 				negative.add(nums[i]);
 			}
 		}
-		if(negative.size()>0) {
-			throw new Exception("negatives not allowed: " + String.join(", ",negative));
+		if (negative.size() > 0) {
+			throw new Exception("negatives not allowed: " + String.join(", ", negative));
 		}
 	}
 }
--- Calculator/src/com/incubyte/calculator/test/CalculatorTest.java
+++ Calculator/src/com/incubyte/calculator/test/CalculatorTest.java
@@ -1,19 +1,20 @@
 package com.incubyte.calculator.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.jupiter.api.Test;
 
 import com.incubyte.calculator.Calculator;
-import static org.junit.Assert.*;
-
-import org.junit.jupiter.api.Test; 
 
 /**
  * Test class for calculator
+ * 
  * @author shishir
  *
  */
 public class CalculatorTest {
-	
-	
+
 	/**
 	 * Test Method to add two numbers separated by a ,
 	 */
@@ -26,7 +27,7 @@ public class CalculatorTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Test Method to add an empty string
 	 */
@@ -39,7 +40,7 @@ public class CalculatorTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Test Method check if a number is passed
 	 */
@@ -52,7 +53,7 @@ public class CalculatorTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Test Method to add four numbers separated by a ,
 	 */
@@ -65,26 +66,59 @@ public class CalculatorTest {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Method that tests on exception for negative numbers
+	 */
 	@Test
-	public void shouldThrowExceptionForNegativeNumbers() {
-		try{
-				Calculator.add("1,-2,3");
-				fail("Exception expected");
-		} catch(Exception e) {
+	public void testThrowExceptionForNegativeNumbers() {
+		try {
+			Calculator.add("1,-2,3");
+			fail("Exception expected");
+		} catch (Exception e) {
+			System.out.println("Negatives not allowed");
+		}
+	}
+
+	/**
+	 * Test Exception message should have negative number
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testHaveNegativeNumbersInException() throws Exception {
+		try {
+			Calculator.add("-1,-2,3");
+			fail("Exception expected");
+		} catch (Exception e) {
+			assertEquals("negatives not allowed: -1, -2", e.getMessage());
+		}
+	}
+
+	/**
+	 * Allow custom delimiter by checking // in beginning of first line
+	 */
+	@Test
+	public void testAllowCustomDelimiterByCheckingFirstPosition() {
+		try {
+			assertEquals(3, Calculator.add("//;\n1;2"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	//	TODO: Exception message should have negative number
+	/**
+	 * Custom Delimiter can be custom Regex Character
+	 */
 	@Test
-	public void shouldHaveNegativeNumbersInException() throws Exception {
-		try{
-			Calculator.add("-1,-2,3");
-			fail("Exception expected");
-		}catch(Exception e) {
-			assertEquals("negatives not allowed: -1, -2",e.getMessage());
+	public void testAllowCustomDelimiter() {
+		try {
+			assertEquals(3, Calculator.add("//.\n1.2"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-	
+
 }
