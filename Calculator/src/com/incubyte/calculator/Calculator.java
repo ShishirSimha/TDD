390c1fcd639825f112922d94e98fb58a93ab4127
Shishir Simha <simbashishir@hotmail.com>
2022-03-20 21:44:44 +0530

    Added Calculator classes and test classes

--- Calculator/src/com/incubyte/calculator/Calculator.java
+++ Calculator/src/com/incubyte/calculator/Calculator.java
@@ -1,5 +1,45 @@
 package com.incubyte.calculator;
 
 public class Calculator {
+	
+	/**
+	 * Method that adds up two or more numbers
+	 * @param nums
+	 * @return
+	 */
+	public static int add(String nums) {
+		int answer = 0;
+		//split the numbers
+		String[] numbers = splitNumbers(nums);
+		
+		//throw exception if 1 or more numbers are negative
+		
+		//compute answer
+		answer = computeAnswer(numbers);
+		return answer;
+	}
+	
+	/**
+	 * Method that adds all the numbers
+	 * @param numbers
+	 * @return
+	 */
+	private static int computeAnswer(String[] numbers) {
+		// TODO Auto-generated method stub
+		int answer = 0;
+		for(String number : numbers) {
+			int num = Integer.parseInt(number);
+			answer += num;
+		}
+		return answer;
+	}
 
+	/**
+	 * Method that splits numbers based on a splitter.
+	 * @param nums
+	 * @return
+	 */
+	private static String[] splitNumbers(String nums) {
+		return nums.split(",|\n");
+	}
 }
--- Calculator/src/com/incubyte/calculator/test/CalculatorTest.java
+++ Calculator/src/com/incubyte/calculator/test/CalculatorTest.java
@@ -1,5 +1,25 @@
 package com.incubyte.calculator.test;
 
-public class CalculatorTest {
 
+import com.incubyte.calculator.Calculator;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test; 
+
+/**
+ * Test class for calculator
+ * @author shishir
+ *
+ */
+public class CalculatorTest {
+	
+	
+	/**
+	 * Test Method to add two numbers separated by a ,
+	 */
+	@Test
+	public void testAddTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
+	
 }
